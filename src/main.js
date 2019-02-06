5a3cd0370b81889f9742cfeb202371941823f34e
ONE_IS_ALL <1095772603@qq.com>
2019-02-06 20:19:05 +0800

    'app骨架'

--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 引入全局css
 import 'common/stylus/index.styl'
 import 'babel-polyfill'
+// 移动端点击300ms延迟解决方案
 import fastclick from 'fastclick'
 fastclick.attach(document.body)
 Vue.config.productionTip = false
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,34 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import rank from '@/components/rank/rank'
+import recommend from '@/components/recommend/recommend'
+import search from '@/components/search/search'
+import singer from '@/components/singer/singer'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      redirect: 'recommend'
+    },
+    {
+      path: '/singer',
+      component: singer
+    },
+    {
+      path: '/rank',
+      component: rank
+    },
+    {
+      path: '/search',
+      component: search
+    },
+    {
+      path: '/recommend',
+      component: recommend
     }
   ]
 })
