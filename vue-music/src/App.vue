68bb322b046d2334c3d695cb7e50f8fa652400c0
ONE_IS_ALL <1095772603@qq.com>
2019-02-06 16:39:29 +0800

    '架构'

--- vue-music/src/App.vue
+++ vue-music/src/App.vue
@@ -1,7 +1,9 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
+    <!-- <img src="./assets/logo.png">
+    111
+    <router-view/> -->
+    22222
   </div>
 </template>
 
@@ -11,13 +13,8 @@ export default {
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped lang="stylus" rel="stylesheet/stylus">
+  @import '~common/stylus/variable'
+  #app
+    color: $color-theme
 </style>
