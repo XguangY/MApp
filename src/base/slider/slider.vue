6f07e52f275d0903aec2dd7138b5e7e53fc7df60
ONE_IS_ALL <1095772603@qq.com>
2019-02-08 18:15:39 +0800

    '前端代理抓取歌单数据'

--- src/base/slider/slider.vue
+++ src/base/slider/slider.vue
@@ -51,6 +51,9 @@ export default {
       this.slider.refresh()
     })
   },
+  destroyed () {
+    clearTimeout(this.timer)
+  },
   methods: {
     _setSliderWidth (isResize) {
       this.children = this.$refs.sliderGroup.children
--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -22,7 +22,7 @@
 
 <script type="text/ecmascript-6">
 import Slider from 'base/slider/slider'
-import {getRecommend} from 'api/recommend'
+import {getRecommend, getDiscList} from 'api/recommend'
 import {ERR_OK} from 'api/config'
 
 export default {
@@ -33,6 +33,7 @@ export default {
   },
   created () {
     this._getRecommend()
+    this._getDiscList()
   },
   components: {
     Slider
@@ -43,6 +44,14 @@ export default {
         .then(res => {
           if (res.code === ERR_OK) {
             this.recommends = res.data.slider
+          }
+        })
+    },
+    _getDiscList () {
+      getDiscList()
+        .then(res => {
+          if (res.code === ERR_OK) {
+            console.log(res.data.list)
           }
         })
     }
