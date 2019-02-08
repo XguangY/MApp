0d9d85b3a8ac25fdaa8d6d2cbf6c3baff6480398
ONE_IS_ALL <1095772603@qq.com>
2019-02-08 15:53:00 +0800

    '轮播图组件'

--- src/components/recommend/recommend.vue
+++ src/components/recommend/recommend.vue
@@ -1,7 +1,17 @@
 <template>
   <div class="recommend">
       <div class="recommend-content">
-          <div class="slider-wrapper"></div>
+          <div class="slider-wrapper">
+            <div v-if="recommends.length" class="slider-content">
+            <slider>
+              <div v-for="(item, index) in recommends" :key="index">
+                <a :href="item.linkUrl">
+                  <img :src="item.picUrl">
+                </a>
+              </div>
+            </slider>
+            </div>
+          </div>
           <div class="recommend-list">
               <h1 class="list-title">热门歌单推荐</h1>
               <ul></ul>
@@ -11,19 +21,28 @@
 </template>
 
 <script type="text/ecmascript-6">
+import Slider from 'base/slider/slider'
 import {getRecommend} from 'api/recommend'
 import {ERR_OK} from 'api/config'
 
 export default {
+  data () {
+    return {
+      recommends: []
+    }
+  },
   created () {
     this._getRecommend()
+  },
+  components: {
+    Slider
   },
   methods: {
     _getRecommend () {
       getRecommend()
         .then(res => {
           if (res.code === ERR_OK) {
-            console.log(res.data.slider)
+            this.recommends = res.data.slider
           }
         })
     }
